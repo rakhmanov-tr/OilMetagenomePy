6ea3e85c1657d5f001620d8b90fc91eb961c13ed
Rakhmanov Timur <92419757+rakhmanov-tr@users.noreply.github.com>
2023-02-08 13:29:45 +0300

    Update cli.py

--- OilMetagenomesDBCheck/cli.py
+++ OilMetagenomesDBCheck/cli.py
@@ -9,17 +9,8 @@ from pathlib import Path
 @click.argument("dataset", type=click.Path(exists=True))
 @click.argument("schema", type=click.Path(exists=True))
 @click.option("-v", "--validity", is_flag=True, help="Turn on schema checking.")
-@click.option(
-    "-d", "--duplicate", is_flag=True, help="Turn on line duplicate line checking."
-)
-@click.option("-i", "--doi", is_flag=True, help="Turn on DOI duplicate checking.")
 @click.option("-m", "--markdown", is_flag=True, help="Output is in markdown format")
-@click.option(
-    "-dc",
-    "--duplicated_entries",
-    type=str,
-    help="Commma separated list of columns to check for duplicated entries",
-)
+
 def cli(no_args_is_help=True, **kwargs):
     """\b
     ancientMetagenomeDirCheck: Performs validity check of ancientMetagenomeDir datasets
